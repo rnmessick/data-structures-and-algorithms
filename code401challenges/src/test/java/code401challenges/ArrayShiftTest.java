8178a496cc53c05d4d1b17ea2948f944d3b11d5a
messickr <rnmessick@gmail.com>
2019-08-07 09:21:54 -0700

    added fail test

--- code401challenges/src/test/java/code401challenges/ArrayShiftTest.java
+++ code401challenges/src/test/java/code401challenges/ArrayShiftTest.java
@@ -15,6 +15,18 @@ public class ArrayShiftTest {
     }
 
     @Test
+    public void testArrayShift_fail() {
+        assertFalse("Insert should be at middle not end", new int[]{1, 2, 3, 4, 5}, ArrayShift.arrayShift(new int[]{1, 2, 3, 4}, 5));
+        assertFalse("Insert should be at middle not beginning", new int[]{5, 1, 2, 3, 4}, ArrayShift.arrayShift(new int[]{1, 2, 3, 4}, 5));
+        assertFalse("Insert should not be before middle index on odd length arrays", new int[]{1, 2, 5, 3, 4, 5}, ArrayShift.arrayShift(new int[]{1, 2, 3, 4, 5}, 5));
+
+
+    }
+
+    private void assertFalse(String s, int[] ints, int[] arrayShift) {
+    }
+
+    @Test
     public void testArrayShift_edgeCases() {
         assertArrayEquals("Should insert value in empty array", new int[]{5}, ArrayShift.arrayShift(new int[]{}, 5));
         assertArrayEquals("Should insert value in array with only one element", new int[]{4, 5}, ArrayShift.arrayShift(new int[]{4}, 5));
