4e43e33457f54690701364252d87c85fc96f732f
messickr <rnmessick@gmail.com>
2019-08-07 07:45:56 -0700

    All tests pass for array shift

--- code401challenges/src/main/java/code401challenges/ArrayShift.java
+++ code401challenges/src/main/java/code401challenges/ArrayShift.java
@@ -2,6 +2,19 @@ package code401challenges;
 
 public class ArrayShift {
     public static int[] arrayShift(int[] arrToBeShifted, int intToBeAdded) {
+        int[] shiftedArr = new int[arrToBeShifted.length + 1];
+        int middleIdx = (int) Math.ceil(arrToBeShifted.length / 2.0); // turns out this must be a decimal
 
+        for (int i = 0; i < shiftedArr.length; i++) {
+            if (i < middleIdx) {
+                shiftedArr[i] = arrToBeShifted[i];
+            } else if (i == middleIdx) {
+                shiftedArr[i] = intToBeAdded;
+
+            } else {
+                shiftedArr[i] = arrToBeShifted[i - 1];
+            }
+        }
+        return shiftedArr;
     }
 }
--- code401challenges/src/test/java/code401challenges/ArrayShiftTest.java
+++ code401challenges/src/test/java/code401challenges/ArrayShiftTest.java
@@ -7,6 +7,17 @@ import static org.junit.Assert.*;
 public class ArrayShiftTest {
 
     @Test
-    public void arrayShift() {
+    public void testArrayShift() {
+        assertArrayEquals("Insert element in center for even arrays", new int[]{1, 2, 5, 3, 4}, ArrayShift.arrayShift(new int[]{1, 2, 3, 4}, 5));
+        assertArrayEquals("Insert element after middle element for odd length arrays", new int[]{1, 2, 3, 5, 4, 5}, ArrayShift.arrayShift(new int[]{1, 2, 3, 4, 5}, 5));
+
+
+    }
+
+    @Test
+    public void testArrayShift_edgeCases() {
+        assertArrayEquals("Should insert value in empty array", new int[]{5}, ArrayShift.arrayShift(new int[]{}, 5));
+        assertArrayEquals("Should insert value in array with only one element", new int[]{4, 5}, ArrayShift.arrayShift(new int[]{4}, 5));
+
     }
 }
