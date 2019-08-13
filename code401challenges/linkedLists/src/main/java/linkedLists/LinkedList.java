e9092dee7fb2455c64ed6b8c35d52dacabbb7f27
messickr <rnmessick@gmail.com>
2019-08-12 22:27:20 -0700

    refactored during code review, completed lab. All tests pass

--- code401challenges/linkedLists/src/main/java/linkedLists/LinkedList.java
+++ code401challenges/linkedLists/src/main/java/linkedLists/LinkedList.java
@@ -1,7 +1,34 @@
 package linkedLists;
 
-public class LinkedList {
-    Node head; // head of list
+public class LinkedList<T> {
+    Node<T> head; // head of list
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    // Method to insert a new node
+    public LinkedList Insert(T value) {
+        LinkedList list = new LinkedList();
+        Node newNode = new Node(value, this.head);
+        this.head = newNode;
+
+//        System.out.println(list);
+        return list;
+    }
+
+    // Method to see if list contains a value
+    public boolean Includes(T value) {
+        Node current = this.head;
+        while (current != null) {
+            if (current.getData() == value) {
+                return true;
+
+            }
+            current = current.getNext();
+        }
+        return false;
+    }
 
     public boolean isEmpty() {
         return length() == 0;
@@ -11,73 +38,52 @@ public class LinkedList {
         int length = 0;
         Node current = head;
 
-        while(current != null){
-            length ++;
+        while (current != null) {
+            length++;
 
             current = current.next;
         }
         return length;
     }
 
-   // Method to insert a new node
-    public LinkedList Insert(LinkedList list, String data)
-    {
-        // Create a new node with given data
-        Node newNode = new Node(data);
-        newNode.next = null;
-
-        // if linked list is empty, new node is head
-        if (list.head == null) {
-            list.head = newNode;
-        }
-        else {
-            // traverse until the last node and insert new node there
-            Node last = list.head;
-            while (last.next != null) {
-                last = last.next;
-            }
-            //insert new node at last node
-            last.next = newNode;
-        }
-
-        // return list by head
-        return list;
-    }
-
     // Method to stringify lists
     public String toString() {
-        String result = "";
-        Node current = head;
-        while(current.getNext() != null){
-            result += current.getData();
-            if(current.getNext() != null){
-                result += ", ";
-            }
+        String result = "HEAD";
+        Node current = this.head;
+        while (current != null) {
+            result += "->" + current.getData();
             current = current.getNext();
+
         }
-         return "List: " + result;
+        result += "->null";
+        return "List: " + result;
     }
-    static class Node {
+
+    static class Node<T> {
+        private T value;
         String data;
         Node next;
 
         // Constructor to create a new node
         // Next is by default initialized as null
-        Node(String d)
-        {
-            data = d;
-            next = null;
+        public Node(T value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+
         }
 
         public Node getNext() {
             return next;
         }
 
-        public String getData() {
-            return data;
+        public T getData() {
+            return this.value;
         }
     }
-
 }
 
 
