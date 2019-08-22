648e667c78f0833f06b8fe8778808430b7597883
messickr <rnmessick@gmail.com>
2019-08-22 13:21:02 -0700

    Needs serious refactoring

---
+++ code401challenges/src/main/java/code401challenges/utilities/MultiBracketValidation.java
@@ -0,0 +1,24 @@
+package code401challenges.utilities;
+
+import code401challenges.stacksandqueues.Stack;
+
+import java.util.ArrayList;
+
+public class MultiBracketValidation {
+    ArrayList<String> brackets = new ArrayList<>();
+
+    public  static  boolean multiBracketValidation(String input) {
+        if(input.length() == 0) {
+            throw new IllegalArgumentException("String length should be longer than 0");
+        }
+        if((input.length() % 2) != 0) {
+            return false;
+        }
+        Stack<Character> characterStack = new Stack<>();
+
+        for(int i = 0; i < input.length(); i++) {
+            if()
+
+    }
+
+}
